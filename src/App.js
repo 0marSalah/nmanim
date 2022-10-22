c32c312265bd672639f629c9dcfbbb24ac6943b8
Omar Salah <omar.salah1597@gmail.com>
2022-10-22 14:49:00 +0200

    Create About Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import About from './components/About';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-    <Header />
+      <Header />
+      <About />
     </div>
   );
 }
